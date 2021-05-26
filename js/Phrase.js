73d72c54c04d0b952579dcb60d676f38e6626a84
karla-walker <indiablonix@gmail.com>
2021-05-25 19:14:59 -0700

    Created Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,35 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+  addPhraseToDisplay() {
+    const phraseLetters = this.phrase.split('');
+    const ul = document.querySelector('#phrase ul');
+
+    const listItems = phraseLetters
+      .map(letter => {
+        if (letter === ' ') {
+          return `<li class="space">${letter}</li>`;
+        } else {
+          return `<li class="hide letter">${letter}</li>`;
+        }
+      })
+      .forEach(item => ul.insertAdjacentHTML('beforeend', item));
+
+    return listItems;
+  }
+
+  //checks to see if the letter selected by the player matches a letter in the phrase
+  checkLetter(selectedLetter) {}
+
+  // reveals the letter(s) on the board that matches the player's selection
+  showMatchedLetter() {}
+}
+
+const test = new Phrase('Hello world');
+test.addPhraseToDisplay();
