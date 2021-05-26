71344e56934c32b56538d03b186f0992ef84440e
karla-walker <indiablonix@gmail.com>
2021-05-25 23:47:58 -0700

    Created Game class, constructor and function that returns a random phrase object

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,37 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = [
+      new Phrase('I am no Jedi'),
+      new Phrase('may the force be with you'),
+      new Phrase('scruffy looking nerfherder'),
+      new Phrase('never tell me the odds'),
+      new Phrase('I find your lack of faith disturbing'),
+    ];
+    this.activePhrase = null;
+  }
+
+  // hides start screen overlay and initializes game
+  // startGame() {
+  //   const overlay = document.querySelector('#overlay');
+  //   overlay.style.display = 'none';
+  //   this.activePhrase.addPhraseToDisplay();
+  // }
+  // get activePhrase() {
+  //   return this.activePhrase;
+  // }
+
+  /**
+   * Selects random phrase from phrases property
+   * @return {Object} Phrase object chosen to be used
+   */
+  getRandomPhrase() {
+    const randomNum = Math.floor(Math.random() * this.phrases.length);
+    this.activePhrase = this.phrases[randomNum];
+    return this.activePhrase;
+  }
+}
