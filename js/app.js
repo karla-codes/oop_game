1263af01ddf82fc21ac55087b100f3f5a9a9bf07
karla-walker <indiablonix@gmail.com>
2021-05-26 00:23:06 -0700

    Added click event to 'start game' button that creates a new game object and starts a new game

--- js/app.js
+++ js/app.js
@@ -2,17 +2,16 @@
  * Project 4 - OOP Game App
  * app.js */
 
-// console.log(testGame.activePhrase);
-// const phrases = [
-//   new Phrase('I love JavaScript'),
-//   new Phrase('Awesome sauce'),
-//   new Phrase('Code rules'),
-//   new Phrase('code and chill'),
-//   new Phrase('console log'),
-// ];
-
 // const phrase = new Phrase();
 // const game = new Game();
+// game.startGame();
+// console.log(game.activePhrase.phrase);
 // game.getRandomPhrase().addPhraseToDisplay();
 
 // console.log(game.phrases);
+let game;
+const startGameBtn = document.querySelector('#btn__reset');
+
+startGameBtn.addEventListener('click', e => {
+  game = new Game().startGame();
+});
