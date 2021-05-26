534cd66bb94b88964a47e3fed58f02edcb6e5900
karla-walker <indiablonix@gmail.com>
2021-05-25 23:49:14 -0700

    Test

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+// console.log(testGame.activePhrase);
+// const phrases = [
+//   new Phrase('I love JavaScript'),
+//   new Phrase('Awesome sauce'),
+//   new Phrase('Code rules'),
+//   new Phrase('code and chill'),
+//   new Phrase('console log'),
+// ];
+
+function logRandomPhrase(phrase) {
+  console.log(phrase.phrase);
+}
+
+const game = new Game();
+
+logRandomPhrase(game.getRandomPhrase());
+logRandomPhrase(game.getRandomPhrase());
+logRandomPhrase(game.getRandomPhrase());
+logRandomPhrase(game.getRandomPhrase());
+
+// console.log(game.phrases);
+// const phrase = new Phrase();
